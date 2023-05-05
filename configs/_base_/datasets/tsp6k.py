f4ea525e21c9c1e2a4f957b45e0492167c704691
jpt <pt.jiang@mail.nankai.edu.cn>
2023-05-05 14:21:39 +0800

    .

--- configs/_base_/datasets/tsp6k.py
+++ configs/_base_/datasets/tsp6k.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'TSP6kDataset'
-data_root = '/home/jpt/Project/dataset/TSP6K/'
+data_root = './data/TSP6K/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 1024)
--- runall.sh
+++ runall.sh
@@ -1,12 +1,12 @@
-# bash tools/dist_train.sh \
-#     configs/tsp6k/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads.py 8 --auto-resume  
+bash tools/dist_train.sh \
+    configs/tsp6k/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads.py 8 --auto-resume  
 
 # bash tools/dist_test.sh \
 #     configs/tsp6k/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads.py \
 #     ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/latest.pth \
 #     1 --out ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/results.pkl --aug-test --eval mIoU  
 
-bash tools/dist_test.sh \
-    configs/tsp6k/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads.py \
-    ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/latest.pth \
-    1 --out ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/results.pkl --aug-test --eval cityscapes 
+# bash tools/dist_test.sh \
+#     configs/tsp6k/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads.py \
+#     ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/latest.pth \
+#     1 --out ./work_dirs/segnext_base_1024x1024_160k_tsp6k_msaspp_rrm_5tokens_12heads/results.pkl --aug-test --eval cityscapes 
